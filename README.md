6288d62d29f136c85f0d295bf1f62e2bcbdf349c
Thomas Offereins <thomas@mybit.nl>
2018-03-30 12:29:48 +0200

    Initial push

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # lemonjs
+A JavaScript value object for a date range.
+
+## Installation
+
+### Bower
+`bower install lemonjs`
+
+### NPM
+`npm install lemonjs`
+
+## Usage
+
+### HTML
+```html
+<html>
+<head>
+	<title>Hello $lemon.space</title>
+</head>
+<body>
+	<h1>Hello $lemon.space</h1>
+	<input type="text" input="$lemon.space" />
+</body>
+</html>
+```
+
+### JavaScript
+From a DateRange you can get all kinds of interesting information about the timespan.
+
+```javascript
+var scope = { space: "world" };
+
+Lemon(document.documentElement, scope);
+```
+
+## Support
+
+You can reach me via Twitter: @tsoffereins
+
+Please file issues here at GitHub.
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+
+
+/**
+ * Combine all library files into one js file.
+ * 
+ * @return void
+ */
+function cleanDist()
+{
+	gulp.src('./dist/*', { read: false })
+		.pipe(clean());
+}
+
+gulp.task('clean-dist', cleanDist);
+
+// ----------------------------------------------------------------------------
+
+/**
+ * Deploy the main file to the dist folder.
+ * 
+ * @return void
+ */
+function deploy()
+{
+	gulp.src('./src/lemon.js')
+		.pipe(gulp.dest('./dist/'))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(minify({
+			ext: { min: '.min.js' }
+		}))
+		.pipe(gulp.dest('./dist/'));
+}
+
+gulp.task('default', ['clean-dist'], deploy);
+
+// ----------------------------------------------------------------------------
